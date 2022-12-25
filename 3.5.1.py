8a87b299bb0d12718e6ab119d34a0e188f5355f3
krist <kristinalysenko3003@gmail.com>
2022-12-26 01:49:47 +0500

    Выполнен перенос курса валют в БД

--- 3.5.1.py
+++ 3.5.1.py
@@ -1,5 +1,13 @@
 import pandas as pd
 import sqlite3
 
-df = pd.read_csv('currency.csv')
-df.to_sql('currency', sqlite3.connect('currency.sqlite'), if_exists='replace', index=False)
+
+def get_sql():
+    """
+    Переводит csv-файл в таблицу sqlite.
+    """
+    df = pd.read_csv('currency.csv')
+    df.to_sql('currency', sqlite3.connect('currency.sqlite'), if_exists='replace', index=False)
+
+
+get_sql()
