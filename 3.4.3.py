7587f89f1ba85ae7d1332ebe467eb10a7d2078da
krist <kristinalysenko3003@gmail.com>
2022-12-29 14:47:05 +0500

    Получена статистика по региону и обновлена по годам

--- 3.4.3.py
+++ 3.4.3.py
@@ -9,41 +9,51 @@ import pathlib
 import pdfkit
 
 list_print1 = ['Динамика уровня зарплат по годам: ','Динамика количества вакансий по годам: ',
-                      'Динамика уровня зарплат по годам для выбранной профессии: ','Динамика количества вакансий по годам для выбранной профессии: ',
-                      'Уровень зарплат по городам (в порядке убывания): ','Доля вакансий по городам (в порядке убывания): ']
+               'Динамика уровня зарплат по годам для выбранной профессии: ', 'Динамика количества вакансий по годам для выбранной профессии: ',
+               'Уровень зарплат по городам (в порядке убывания): ', 'Доля вакансий по городам (в порядке убывания): ',
+               'Динамика уровня зарплат по годам для выбранной профессии и региона: ',
+               'Динамика количества вакансий по годам для выбранной профессии и региона: ']
 
 
 class Solution:
     """Класс для получения и печати статистик
+
     Attributes:
         path_to_file (str): Путь к входному csv-файлу
         name_vacancy (str): Название выбранной профессии
+        area_name (str): Название выбранного региона
         dynamics1 (dict): Динамика уровня зарплат по годам
         dynamics2 (dict): Динамика количества вакансий по годам
         dynamics3 (dict): Динамика уровня зарплат по годам для выбранной профессии
         dynamics4 (dict): Динамика количества вакансий по годам для выбранной профессии
         dynamics5 (dict): Уровень зарплат по городам (в порядке убывания)
         dynamics6 (dict): Доля вакансий по городам (в порядке убывания)
+        dynamics7 (dict): Динамика уровня зарплат по годам для выбранной профессии и региона
+        dynamics8 (dict): Динамика количества вакансий по годам для выбранной профессии и региона
     """
-    def __init__(self, path_to_file, name_vacancy):
+    def __init__(self, path_to_file, name_vacancy, area_name):
         """Инициализирует объект Solution.
+
         Args:
             name_vacancy (str): Название выбранной профессии
             path_to_file (str): Путь к входному csv-файлу
+            area_name (str): Название выбранного региона
         """
         self.path_to_file = path_to_file
         self.name_vacancy = name_vacancy
+        self.area_name = area_name
         self.dynamics1 = {}
         self.dynamics2 = {}
         self.dynamics3 = {}
         self.dynamics4 = {}
         self.dynamics5 = {}
         self.dynamics6 = {}
+        self.dynamics7 = {}
+        self.dynamics8 = {}
 
     def split_by_year(self):
         """Разделяет входной файл на меньшие, группирует по годам
         """
-
         data_of_file = pd.read_csv(self.path_to_file)
         data_of_file["year"] = data_of_file["published_at"].apply(lambda x: x[:4])
         data_of_file = data_of_file.groupby("year")
@@ -56,23 +66,27 @@ class Solution:
         """
         self.get_dynamics_by_year_with_multiprocessing()
         self.get_dynamics_by_city()
-        return self.dynamics1, self.dynamics2, self.dynamics3, self.dynamics4, self.dynamics5, self.dynamics6
+        return self.dynamics1, self.dynamics2, self.dynamics3, self.dynamics4, self.dynamics5, self.dynamics6, self.dynamics7, self.dynamics8
 
     def get_statistic_by_year(self, file_csv):
         """Составляет статистику по году
         Args:
             file_csv (str): Название файла с данными о вакансиях за год
         Returns:
-            str, [int, int, int, int]: год, [ср. зп, всего вакансий, ср. зп для профессии, вакансий по профессии]
+            str, [int, int, int, int, int, int]: год, [ср. зп, всего вакансий, ср. зп для профессии,
+            вакансий по профессии, ср. зп для профессии и региона, вакансий по профессии и региону]
         """
         data_of_file = pd.read_csv(file_csv)
         data_of_file["salary"] = data_of_file[["salary_from", "salary_to"]].mean(axis=1)
         data_of_file["published_at"] = data_of_file["published_at"].apply(lambda s: int(s[:4]))
         data_of_file_vacancy = data_of_file[data_of_file["name"].str.contains(self.name_vacancy, case=False)]
+        data_of_file_vacancy_area = data_of_file_vacancy[data_of_file_vacancy["area_name"].str.contains(self.area_name)]
 
         return data_of_file["published_at"].values[0], [int(data_of_file["salary"].mean()), len(data_of_file),
                                                         int(data_of_file_vacancy["salary"].mean() if len(data_of_file_vacancy) != 0 else 0),
-                                                        len(data_of_file_vacancy)]
+                                                        len(data_of_file_vacancy),
+                                                        int(data_of_file_vacancy_area["salary"].mean() if len(data_of_file_vacancy_area) != 0 else 0),
+                                                        len(data_of_file_vacancy_area)]
 
     def get_dynamics_by_year_with_multiprocessing(self):
         """Получает статистики по годам с использованием нескольких процессов
@@ -86,6 +100,8 @@ class Solution:
             self.dynamics2[year] = data_dynamics[1]
             self.dynamics3[year] = data_dynamics[2]
             self.dynamics4[year] = data_dynamics[3]
+            self.dynamics7[year] = data_dynamics[4]
+            self.dynamics8[year] = data_dynamics[5]
 
     def get_dynamics_by_city(self):
         """Получает статистики по городам
@@ -111,7 +127,7 @@ class Solution:
         Prints:
             Печатать каждой динамики с описанием
         """
-        list_print2 = [self.dynamics1, self.dynamics2, self.dynamics3, self.dynamics4, self.dynamics5, self.dynamics6]
+        list_print2 = [self.dynamics1, self.dynamics2, self.dynamics3, self.dynamics4, self.dynamics5, self.dynamics6, self.dynamics7, self.dynamics8]
         for i in range(len(list_print1)):
             print(list_print1[i] + '{0}'.format(list_print2[i]))
         InputConnect(self.path_to_file, self.name_vacancy, self.dynamics1, self.dynamics2, self.dynamics3, self.dynamics4, self.dynamics5, self.dynamics6)
@@ -136,7 +152,6 @@ class InputConnect:
             dynamics4 (dict): Динамика количества вакансий по годам для выбранной профессии
             dynamics5 (dict): Уровень зарплат по городам (в порядке убывания)
             dynamics6 (dict): Доля вакансий по городам (в порядке убывания)
-
         """
         self.path_to_file, self.name_vacancy = path_to_file, name_vacancy
         dynamics1, dynamics2, dynamics3, dynamics4, dynamics5, dynamics6 = dynamics1, dynamics2, dynamics3, dynamics4, dynamics5, dynamics6
@@ -249,13 +264,14 @@ class Report:
                                         'dynamics': dynamics, 'dynamics5': self.dynamics5, 'dynamics6': self.dynamics6})
 
         config = pdfkit.configuration(wkhtmltopdf=r'C:\Program Files\wkhtmltopdf\bin\wkhtmltopdf.exe')
-        pdfkit.from_string(pdf_template, 'report_3_4_2.pdf', configuration=config, options={"enable-local-file-access": ""})
+        pdfkit.from_string(pdf_template, 'report_3_4_3.pdf', configuration=config, options={"enable-local-file-access": ""})
 
 
 if __name__ == '__main__':
     filename = input('Введите название файла: ')
     name_vacancy = input('Введите название профессии: ')
-    solve = Solution(filename, name_vacancy)
+    area_name = input('Введите название региона: ')
+    solve = Solution(filename, name_vacancy, area_name)
     solve.split_by_year()
     solve.get_dynamics()
     solve.print_statistic()
